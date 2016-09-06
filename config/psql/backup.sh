44e8d46c23f559aebeb83f1cb176329d3b9b0c88
online <man@tz.world>
2016-09-06 14:40:29 +0000

    f

--- config/psql/backup.sh
+++ config/psql/backup.sh
@@ -13,4 +13,4 @@ $PREFIX/pg_rman/$psql
 # sudo -H -u postgres borg init $BACKUP/psql_borg
 sudo -H -u postgres borg create  --compression lzma,3 -v --stats $BACKUP/psql_borg::backup-`date +%Y-%m-%d` $BACKUP/psql
 sudo -H -u postgres borg prune -v $BACKUP/psql_borg --prefix backup- --keep-daily=7 --keep-weekly=4 --keep-monthly=1200
-sudo -H -u postgres b2upload --bucket psql-borg --directory /data/backup/psql_borg
+sudo -H -u postgres rclone sync /data/backup/psql_borg/ b2:tzol-backup/psql_borg
