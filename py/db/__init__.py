3abab5b406ea3643cf1bb560b5eee277de084def
noman <man@tz.world>
2016-09-16 11:43:28 +0800

    f

--- py/db/__init__.py
+++ py/db/__init__.py
@@ -2,14 +2,9 @@ from ztz.db.sync.q import QDb
 from ztz.db.sync_patch import MongoClient
 from config import CONFIG
 
-MONGO_URL = "mongodb://%s:%s@%s:%s/?authMechanism=SCRAM-SHA-1" % (
-    CONFIG.MONGO.USER,
-    CONFIG.MONGO.PASSWORD,
-    CONFIG.MONGO.HOST,
-    CONFIG.MONGO.PORT
-)
-MONGO_DB = MongoClient(MONGO_URL)
+MONGO_DB = MongoClient(CONFIG.MONGO.HOST + ":" + str(CONFIG.MONGO.PORT))
 DB = getattr(MONGO_DB, CONFIG.MONGO.DB)
+DB.authenticate(CONFIG.MONGO.USER, CONFIG.MONGO.PASSWORD, mechanism='SCRAM-SHA-1')
 
 Q = QDb(DB)
 
