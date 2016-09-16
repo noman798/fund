0541540a51064eebb6807bcffb2cf8152a53e5e8
noman <man@tz.world>
2016-09-16 11:24:09 +0800

    f

--- sync/db.py
+++ sync/db.py
@@ -1,7 +1,17 @@
 from ztz.db.sync.q import QDb
+from ztz.db.sync_patch import MongoClient
+from config import CONFIG
 
+MONGO_URL = "mongodb://%s:%s@%s:%s/" % (
+    CONFIG.MONGO.USER,
+    CONFIG.MONGO.PASSWORD,
+    CONFIG.MONGO.HOST,
+    CONFIG.MONGO.PORT
+)
+MONGO_DB = MongoClient(MONGO_URL)
+DB = getattr(MONGO_DB, CONFIG.MONGO.DB)
+
+Q = QDb(DB)
 
 if __name__ == "__main__":
     pass
-    # main()
-
