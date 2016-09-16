f692e003e2a1a39510fbf242941f81ad3460b34e
noman <man@tz.world>
2016-09-16 12:52:37 +0800

    f

--- py/db/qq.py
+++ py/db/qq.py
@@ -4,7 +4,7 @@ from db import Q
 
 def qq_save(qq):
     qq = int(qq)
-    o = Q.get(qq=qq)
+    o = Q.QQ.get(qq=qq)
     if not o:
-        o = Q.upsert(qq=qq)(_id=gid())
+        o = Q.QQ.upsert(qq=qq)(_id=gid())
     return o._id
