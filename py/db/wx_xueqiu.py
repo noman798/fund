4b8d3d869aa5f8c125ace4f488bb453ec0a0327e
noman <man@tz.world>
2016-09-16 15:47:21 +0800

    f

--- py/db/wx_xueqiu.py
+++ py/db/wx_xueqiu.py
@@ -8,6 +8,7 @@ from ztz.util.url_short import url_short
 from binascii import b2a_hex
 
 RE_BR = re.compile(r'\s*<br\s*/?\s*>\s*', re.I)
+RE_STYLE = re.compile(r'\s+style="[^"]*"', re.I)
 
 
 def wx_xueqiu_post_save(user_id, post_id):
@@ -19,21 +20,20 @@ def wx_xueqiu_post_save(user_id, post_id):
 
 def wx_xueqiu_sync():
     pre_user_id = 0
-    for i in Q.WxXueqiuPost.find(
-        xueqiu__exists=False
-    ).iter(
+    for wx_xueqiu_post in Q.WxXueqiuPost.iter(
+        xueqiu__exists=False,
         sort=[['user_id', 1], ['_id', -1]]
     ):
-        post = Q.WxPost.get(i.post_id)
+
+        post = Q.WxPost.get(wx_xueqiu_post.post_id)
         html = post.html
-        html = RE_BR.sub('', html).replace(
+        html = RE_STYLE.sub('', RE_BR.sub('', html)).replace(
             "<p></p>", "<br>"
         ).replace(
             "</p><br>", "</p>"
-        )
+        ).replace("<span>", '').replace("</span>", '').strip()
         if post.desc and post.desc not in html:
             html = ("<p>%s</p>─────────────────<br>" % post.desc) + html
-
         total = len(html)
         limit = 19000
 
@@ -49,26 +49,24 @@ def wx_xueqiu_sync():
             html = html[:limit]
             html = html[:html.rfind("<")]
             html += """<p>… … …</p><p>还有 %s 字，请移步原文链接: <a href="%s">%s</a></p>""" % (
-                len(total) - limit,
+                total - limit,
                 url,
                 url
             )
         else:
-            html = """<p>原文链接 : <a href="%s">%s</a></p>""" % (url, url)
+            html += """<p>原文链接 : <a href="%s">%s</a></p>""" % (url, url)
 
-        if post.author and post.author != wx.wx_name:
+        if post.author and post.author != wx.name:
             html += ("<p>作者 ：%s</p>" % escape(post.author))
 
         html += "<p>转自微信公众号 ：%s ( %s ) </p>" % (wx.name, wx.en)
-
-        if i.user_id != pre_user_id:
+        if wx_xueqiu_post.user_id != pre_user_id:
             xueqiu = Xueqiu()
-            xueqiu.login(*tuple(Q.Qq.get(i.user_id).xueqiu))
+            xueqiu.login(*tuple(Q.Qq.get(wx_xueqiu_post.user_id).xueqiu))
         r = xueqiu.new_post(post.title, html)
         if 'target' in r:
             url = r['target']
-            print(url)
-            Q.WxXueqiuPost.upsert(_id=post._id)(xueqiu=url)
+            Q.WxXueqiuPost.upsert(_id=wx_xueqiu_post._id)(xueqiu=url)
         else:
             print(r)
 
