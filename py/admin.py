b7698bd1246500ff67f7dbe3929b81cf070821ce
noman <man@tz.world>
2016-09-18 09:59:54 +0800

    f

--- py/admin.py
+++ py/admin.py
@@ -1,9 +1,17 @@
-# from time import time
+from config import CONFIG
+import psycopg2
 
-# def main():
-#     pass
+conn = psycopg2.connect(CONFIG.PSQL)
 
-if __name__ == "__main__":
-    pass
-    # main()
-
+c = conn.cursor()
+c.execute("show search_path")
+r = c.fetchone()
+print(r)
+c.execute("SET search_path=public")
+c.execute("show search_path")
+r = c.fetchone()
+print(r)
+c.execute("select id from user where mail=%s", ("man@tz.world",))
+r = c.fetchone()[0]
+print(r)
+#c.execute("INSERT INTO test (num, data) VALUES (%s, %s)")
