12219a4ed0fe2d636074fbfd29cdce6da0e9afee
noman <man@tz.world>
2016-09-18 10:32:15 +0800

    f

--- py/admin.py
+++ py/admin.py
@@ -1,17 +1,15 @@
 from config import CONFIG
 import psycopg2
 
-conn = psycopg2.connect(CONFIG.PSQL)
+CONN = psycopg2.connect(CONFIG.PSQL)
 
-c = conn.cursor()
-c.execute("show search_path")
-r = c.fetchone()
-print(r)
-c.execute("SET search_path=public")
-c.execute("show search_path")
-r = c.fetchone()
-print(r)
-c.execute("select id from user where mail=%s", ("man@tz.world",))
-r = c.fetchone()[0]
-print(r)
-#c.execute("INSERT INTO test (num, data) VALUES (%s, %s)")
+
+def user_admin_new(mail):
+    c = CONN.cursor()
+    c.execute("select id from public.user where mail=%s", (mail,))
+    user_id = c.fetchone()[0]
+    c.execute("INSERT INTO user_admin (id) values (%s) ON CONFLICT DO NOTHING", (user_id,))
+    CONN.commit()
+
+if __name__ == "__main__":
+    user_admin_new("man@tz.world")
