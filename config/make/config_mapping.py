3f9ccc888d1b376d95913c85ca75469494dbd235
noman <man@tz.world>
2016-09-28 14:29:49 +0800

    f

--- config/make/config_mapping.py
+++ config/make/config_mapping.py
@@ -1,14 +1,3 @@
 MAPPING = (
-    ('nginx.conf', "release"),
-    ('fis-conf.js', "pure"),
-    ('const.js', "pure/lib"),
-    ('dev', "code/py"),
-    ('ac_run', "code/d/autocomplete"),
-    ('config.coffee', "code/leancloud"),
-    ('av_run', "code/leancloud"),
-    ('config.py', "code/py/ztz"),
-    ('config.d', "code/d/autocomplete/source"),
-    ('backup.sh', "code/sh"),
-    ('copy', "cli"),
-    ("tzol.conf", 'build/'),
+    ('coffee.coffee', "release"),
 )
